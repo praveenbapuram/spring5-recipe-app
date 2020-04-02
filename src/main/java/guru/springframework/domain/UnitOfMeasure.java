ea96b25deb34f5fe5e3bc3cf0f277014402320fd
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-04-02 15:37:11 +0530

    Adding the Recipe,Ingredients,Notes,UnitOfMeasure entities.

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,29 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String uof;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUof() {
+        return uof;
+    }
+
+    public void setUof(String uof) {
+        this.uof = uof;
+    }
+
+}
